40081ee4a4a7266d27a4cc4d957dc18a7b40e50f
yatatsu <yatatsukitagawa@gmail.com>
2016-12-07 00:28:40 +0900

    feat(input): introduce AutoBundle

--- app/src/main/java/com/yatatsu/expiresmemo/presentation/input/EditActivity.kt
+++ app/src/main/java/com/yatatsu/expiresmemo/presentation/input/EditActivity.kt
@@ -3,11 +3,12 @@ package com.yatatsu.expiresmemo.presentation.input
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.yatatsu.autobundle.AutoBundle
+import com.yatatsu.autobundle.AutoBundleField
 import com.yatatsu.expiresmemo.App
 import com.yatatsu.expiresmemo.R
 import com.yatatsu.expiresmemo.databinding.ActivityInputBinding
 import com.yatatsu.expiresmemo.model.Expire
-import com.yatatsu.expiresmemo.presentation.getStringExtraOrNull
 import java.util.Date
 import javax.inject.Inject
 
@@ -16,21 +17,29 @@ class EditActivity : AppCompatActivity(), EditContract.View {
   @Inject lateinit var presenter: EditContract.Presenter
   private lateinit var binding: ActivityInputBinding
 
-  private var loaded: Boolean = false
+  @AutoBundleField(required = false) var loaded: Boolean = false
+  @AutoBundleField(required = false) var expireId: String? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = DataBindingUtil.setContentView(this, R.layout.activity_input)
     App.get(this).appComponent.plus(EditModule(this)).inject(this)
-    intent.getStringExtraOrNull("id").let {
-      presenter.setId(it)
-    }
+
+    AutoBundle.bind(this, intent)
+    savedInstanceState?.let { AutoBundle.bind(this, it) }
+    presenter.setId(expireId)
+
     binding.buttonCreate.setOnClickListener {
       presenter.saveExpire(binding.expire,
           binding.name.text.toString(),
           binding.description.text.toString(),
           Date())
     }
+  }
+
+  override fun onSaveInstanceState(outState: Bundle?) {
+    super.onSaveInstanceState(outState)
+    AutoBundle.pack(this, outState)
   }
 
   override fun onStart() {
